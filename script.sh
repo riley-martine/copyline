dc767fe1915999b2310f3079c614dccc68eff679
Riley Martine <riley.martine@protonmail.com>
2016-04-18 15:36:30 -0400

    initial commit

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+COUNT=1
+LINES="$(perl -lne 'END { print $. }' ~/hi.txt)" #get total number of lines in file
+
+while [ $LINES -gt $COUNT ]; do
+	read -n 1 -s                             #pause for keypress
+
+	sed "${COUNT}q;d" ~/hi.txt > ~/temp.txt  #get line $COUNT from file
+	xclip -in -selection c ~/temp.txt        #copy to clipboard
+
+	echo line $COUNT / $LINES -- `expr $LINES - $COUNT` left 
+                                                 #say how many left
+	echo "$(xclip -selection c -o)"          #show what has been copied
+
+	rm temp.txt && touch temp.txt            #I'm an idiot I know
+	let COUNT+=1                             #iterate to next line
+done
+
+rm temp.txt                                      #cleanup
+
