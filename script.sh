4fec4c58360d34ccbd5dec6e8fc0a3b0bd0f9bc4
Riley Martine <riley.martine@protonmail.com>
2016-04-18 17:11:45 -0400

    those magic changes

--- script.sh
+++ script.sh
@@ -3,8 +3,8 @@ COUNT=1
 LINES="$(perl -lne 'END { print $. }' ~/hi.txt)" #get total number of lines in file
 
 while [ $LINES -gt $COUNT ]; do
-	read -n 1 -s                             #pause for keypress
-       #sleep 2                                  #other option, waits 2 seconds
+	#read -n 1 -s                             #pause for keypress
+         sleep 1                                  #other option, waits 1 second
 
 	sed "${COUNT}q;d" ~/hi.txt > ~/temp.txt  #get line $COUNT from file
 	xclip -in -selection c ~/temp.txt        #copy to clipboard
@@ -13,9 +13,13 @@ while [ $LINES -gt $COUNT ]; do
                                                  #say how many left
 	echo "$(xclip -selection c -o)"          #show what has been copied
 
-	rm temp.txt && touch temp.txt            #I'm an idiot I know
+	rm ~/temp.txt && touch ~/temp.txt        #I'm an idiot I know
 	let COUNT+=1                             #iterate to next line
 done
 
-rm temp.txt                                      #cleanup
+echo line $COUNT / $LINES -- `expr $LINES - $COUNT` left 
+echo success
+echo exiting...
+
+rm ~/temp.txt                                    #cleanup
 
