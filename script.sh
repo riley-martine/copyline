96d4f20dddb8d31dd514dc51bfc93e415f7e8998
Riley Martine <riley.martine@protonmail.com>
2016-04-24 15:01:16 -0400

    fix last line error

--- script.sh
+++ script.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 COUNT=1
-LINES="$(perl -lne 'END { print $. }' ~/hi.txt)" #get total number of lines in file
+LINES="$(perl -lne 'END { print $. }' lines.txt)" #get total number of lines in file
 
-while [ $LINES -gt $COUNT ]; do
+echo "./lines.txt being used"
+touch lines.txt
+touch temp.txt
+
+while [ $LINES -ge $COUNT ]; do
 	#read -n 1 -s                             #pause for keypress
          sleep 1                                  #other option, waits 1 second
 
-	sed "${COUNT}q;d" ~/hi.txt > ~/temp.txt  #get line $COUNT from file
-	xclip -in -selection c ~/temp.txt        #copy to clipboard
+	sed "${COUNT}q;d" lines.txt > temp.txt       #get line $COUNT from file
+	xclip -in -selection c temp.txt           #copy to clipboard
 
 	echo line $COUNT / $LINES -- `expr $LINES - $COUNT` left 
                                                  #say how many left
 	echo "$(xclip -selection c -o)"          #show what has been copied
 
-	rm ~/temp.txt && touch ~/temp.txt        #I'm an idiot I know
-	let COUNT+=1                             #iterate to next line
+	rm temp.txt && touch temp.txt            #I'm an idiot I know
+	let COUNT+=1                           #iterate to next line
 done
 
-echo line $COUNT / $LINES -- `expr $LINES - $COUNT` left 
 echo success
 echo exiting...
 
-rm ~/temp.txt                                    #cleanup
+rm temp.txt                                      #cleanup
 
